9c414edb95c2aa1baa6898b4b41dfc00fc7b95f2
Arvand Molla <mr.arvand@gmail.com>
2021-11-30 19:57:01 +0100

    user selector

--- src/pages/UserSelector.jsx
+++ src/pages/UserSelector.jsx
@@ -3,9 +3,10 @@ import { Form, Button } from "react-bootstrap";
 
 const ApiUrl = process.env.REACT_APP_API_URL;
 
-function UserSelector() {
+function UserSelector({ setSenderId }) {
   const [allUsers, setAllUsers] = useState(null);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [isSelected, setIsSelected] = useState(false);
 
   useEffect(() => {
     fetchAllUsers();
@@ -32,30 +33,63 @@ function UserSelector() {
       alert(error.message);
     }
   };
-  return (
-    <div className="getid-container">
-      <Form.Group>
-        <Form.Label>Select User</Form.Label>
-        <Form.Control
-          as="select"
-          className="user-selector"
-          onChange={(e) => setSelectedUser(e.target.value)}
-        >
-          {allUsers &&
-            allUsers.map((elem) => {
-              return (
-                <option className="select-option" key={elem._id}>
-                  {elem.userName}
-                </option>
-              );
-            })}
-        </Form.Control>
 
-        <Button variant="primary" className="user-select-button">
-          Submit
-        </Button>
-      </Form.Group>
-    </div>
+  const setUserId = () => {
+    allUsers.forEach((elem) => {
+      if (elem.userName == selectedUser) {
+        setSenderId(elem._id);
+      }
+    });
+  };
+  return (
+    <>
+      {isSelected ? (
+        <div className="getid-container">
+          <Form.Group>
+            <Form.Label>
+              Now open a new tab and choose a different user to chat with this
+              user
+            </Form.Label>
+
+            <Button
+              variant="primary"
+              className="user-select-button"
+              onClick={() => setUserId()}
+            >
+              I got it
+            </Button>
+          </Form.Group>
+        </div>
+      ) : (
+        <div className="getid-container">
+          <Form.Group>
+            <Form.Label>Select User</Form.Label>
+            <Form.Control
+              as="select"
+              className="user-selector"
+              onChange={(e) => setSelectedUser(e.target.value)}
+            >
+              {allUsers &&
+                allUsers.map((elem) => {
+                  return (
+                    <option className="select-option" key={elem._id}>
+                      {elem.userName}
+                    </option>
+                  );
+                })}
+            </Form.Control>
+
+            <Button
+              variant="primary"
+              className="user-select-button"
+              onClick={() => setIsSelected(true)}
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </div>
+      )}
+    </>
   );
 }
 
