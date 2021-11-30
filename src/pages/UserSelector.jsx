ec48f22fcc7b631563565cfcee624b69d263e761
Arvand Molla <mr.arvand@gmail.com>
2021-11-30 15:28:06 +0100

    user selector

--- src/pages/UserSelector.jsx
+++ src/pages/UserSelector.jsx
@@ -1,14 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { Container, Form, Alert, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+
+const ApiUrl = process.env.REACT_APP_API_URL;
 
 function UserSelector() {
+  const [allUsers, setAllUsers] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
+  useEffect(() => {
+    if (allUsers) {
+      setSelectedUser(allUsers[0].userName);
+    }
+  }, [allUsers]);
+
+  const fetchAllUsers = async () => {
+    try {
+      const response = await fetch(`${ApiUrl}/api/user/all/all-users`);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setAllUsers(data);
+      } else {
+        console.log("failed");
+        throw new Error("fetching all users failed!");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   return (
-    <>
-      <Form.Control as="select" defaultValue="Choose...">
-        <option>Choose...</option>
-        <option>...</option>
-      </Form.Control>
-    </>
+    <div className="getid-container">
+      <Form.Group>
+        <Form.Label>Select User</Form.Label>
+        <Form.Control
+          as="select"
+          className="user-selector"
+          onChange={(e) => setSelectedUser(e.target.value)}
+        >
+          {allUsers &&
+            allUsers.map((elem) => {
+              return (
+                <option className="select-option" key={elem._id}>
+                  {elem.userName}
+                </option>
+              );
+            })}
+        </Form.Control>
+
+        <Button variant="primary" className="user-select-button">
+          Submit
+        </Button>
+      </Form.Group>
+    </div>
   );
 }
 
